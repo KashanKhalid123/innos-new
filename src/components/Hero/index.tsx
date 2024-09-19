e4de31df32ab3cf2d6ea75a03fda3efa5bf65928
Kashan Khalid <143093682+KashanKhalid123@users.noreply.github.com>
2024-09-19 13:11:54 -0700

    Update index.tsx

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -39,7 +39,7 @@ const Hero = () => {
                       target="_blank"
                       className="flex items-center gap-4 rounded-md bg-white/[0.12] px-6 py-[14px] text-base font-medium text-white transition duration-300 ease-in-out hover:bg-white hover:text-dark"
                     >
-                      Planlegg møte
+                      Book et møte
                     </Link>
                   </li>
                 </ul>
