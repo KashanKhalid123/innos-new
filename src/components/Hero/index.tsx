261a67e14710904198e566a52a1011bf53aa876c
Kashan Khalid <143093682+KashanKhalid123@users.noreply.github.com>
2024-10-14 06:49:50 -0700

    Update index.tsx

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -19,10 +19,10 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-6 text-3xl font-bold leading-snug text-white sm:text-4xl sm:leading-snug lg:text-5xl lg:leading-[1.2]">
-                Velkommen til Innoscribe
+                Innoscribe
                 </h1>
                 <p className="mx-auto mb-9 max-w-[600px] text-base font-medium text-white sm:text-lg sm:leading-[1.44]">
-                Optimaliser bedriften din med AI! <br />
+               Oppdatér bedriften din med AI <br />
                 <span className="text-center"></span></p>
                 <ul className="mb-10 flex flex-wrap items-center justify-center gap-5">
                   <li>
